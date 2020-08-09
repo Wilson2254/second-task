b36d6cc9e33bf4d3ed05d05b045410bd836420c6
Alexander_P <sanek191296@mail.ru>
2020-08-09 19:30:04 +0300

    add rounds

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -3,19 +3,19 @@
     <h1>Раунд: {{this.rounds}}</h1>
     <div class="fields">
       <div
-        :class="{one_field:true, field_choose:this.queue[queue.length-1] == 1}"
+        :class="{one_field:true, field_choose:this.queue[queue.length-1] == 1, stop_click: isPlay}"
         @click.prevent="changeField"
       ></div>
       <div
-        :class="{two_field:true,field_choose:this.queue[queue.length-1] == 2}"
+        :class="{two_field:true,field_choose:this.queue[queue.length-1] == 2, stop_click: isPlay}"
         @click="changeField"
       ></div>
       <div
-        :class="{three_field:true, field_choose:this.queue[queue.length-1] == 3}"
+        :class="{three_field:true, field_choose:this.queue[queue.length-1] == 3, stop_click: isPlay}"
         @click="changeField"
       ></div>
       <div
-        :class="{four_field:true, field_choose:this.queue[queue.length-1] == 4}"
+        :class="{four_field:true, field_choose:this.queue[queue.length-1] == 4, stop_click: isPlay}"
         @click="changeField"
       ></div>
     </div>
@@ -23,7 +23,7 @@
       <a class="button8" @click="startGame" :class="{stop_click: isGame}">Старт</a>
       <h2>Режим: {{this.mode}}</h2>
       <div class="mode">
-        <form>
+        <form :class="{stop_click: isGame}">
           <input type="radio" value="Легкий" name="modeChoose" v-model="mode" />
           <label for="lightMode">Легкий</label>
 
@@ -45,6 +45,7 @@ export default {
       rounds: 0,
       mode: "Легкий",
       queue: [],
+      supportQueue: [Math.floor(Math.random() * (4 - 1 + 1)) + 1],
       isGame: false,
       isPlay: false,
     };
@@ -52,36 +53,31 @@ export default {
   methods: {
     startGame() {
       this.isGame = true;
+      this.isPlay = false;
       this.rounds++;
       this.playGame();
     },
-    playGame(min = 1, max = 4) {
-      let firstElem;
+    playGame() {
+      let firstElem = this.supportQueue[0];
       let track = new Audio();
-      track.pause();
       const some = this;
-      for (let i = 0; i < 5; i++) {
+      for (let i = 0; i < some.rounds; i++) {
         new Promise(function (resolve) {
           setTimeout(
-            () =>
-              resolve(
-                some.queue.push(
-                  Math.floor(Math.random() * (max - min + 1)) + min
-                )
-              ),
+            () => resolve(some.queue.push(some.supportQueue.shift())),
             1400 * i
           );
         })
           .then(function () {
             setTimeout(() => some.queue.push("trash"), 500);
             console.log(some.queue);
-            if (i == 4) {
+            if (i == some.rounds - 1) {
               some.queue.splice(0, 0, firstElem);
+              // some.isPlay = false;
             }
           })
           .then(function () {
             if (some.queue.length == 1) {
-              firstElem = some.queue[0];
               setTimeout(
                 () => some.queue.splice(some.queue.indexOf("trash"), 1),
                 450
@@ -97,33 +93,72 @@ export default {
       }
     },
     changeField(e) {
+      // if (this.queue[0] == undefined) this.queue.shift();
       var el = e.target;
       console.log(el.className);
-      // Просто смотрю что выбрал и меняю шаг
       if (el.className == "one_field") {
         let track = new Audio(
           "http://soundbible.com/mp3/Air Plane Ding-SoundBible.com-496729130.mp3"
         );
         track.play();
+        this.supportQueue.push(this.queue.shift());
+        console.log(this.supportQueue);
         console.log(this.queue);
+        if (this.supportQueue[this.supportQueue.length - 1] == 1)
+          console.log("1");
+        if (this.queue[0] == "trash") {
+          this.supportQueue.push(Math.floor(Math.random() * (4 - 1 + 1)) + 1);
+          this.queue.shift();
+          this.startGame();
+        }
       }
       if (el.className == "two_field") {
         let track = new Audio(
           "http://soundbible.com/mp3/Computer Error Alert-SoundBible.com-783113881.mp3"
         );
         track.play();
+        this.supportQueue.push(this.queue.shift());
+        console.log(this.supportQueue);
+        console.log(this.queue);
+        if (this.supportQueue[this.supportQueue.length - 1] == 2)
+          console.log("2");
+        if (this.queue[0] == "trash") {
+          this.supportQueue.push(Math.floor(Math.random() * (4 - 1 + 1)) + 1);
+          this.queue.shift();
+          this.startGame();
+        }
       }
       if (el.className == "three_field") {
         let track = new Audio(
           "http://soundbible.com/mp3/Electronic_Chime-KevanGC-495939803.mp3"
         );
         track.play();
+        this.supportQueue.push(this.queue.shift());
+        console.log(this.supportQueue);
+        console.log(this.queue);
+        if (this.supportQueue[this.supportQueue.length - 1] == 3)
+          console.log("3");
+        if (this.queue[0] == "trash") {
+          this.supportQueue.push(Math.floor(Math.random() * (4 - 1 + 1)) + 1);
+          this.queue.shift();
+          this.startGame();
+        }
       }
       if (el.className == "four_field") {
         let track = new Audio(
           "http://soundbible.com/mp3/Elevator Ding-SoundBible.com-685385892.mp3"
         );
         track.play();
+        this.supportQueue.push(this.queue.shift());
+        console.log(this.supportQueue);
+        console.log(this.queue);
+        if (this.supportQueue[this.supportQueue.length - 1] == 4)
+          console.log("4");
+        if (this.queue[0] == "trash") {
+          this.supportQueue.push(Math.floor(Math.random() * (4 - 1 + 1)) + 1);
+          this.queue.shift();
+          this.startGame();
+        }
       }
     },
   },
@@ -180,9 +215,9 @@ export default {
   opacity: 0.6;
 }
 
-/* .stop_click {
+.stop_click {
   pointer-events: none;
-} */
+}
 
 .one_field:active,
 .two_field:active,
